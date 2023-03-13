0d03a63a8b8061e48e5be945458ac05ab14435d7
aidenpearce001 <aidenpearcewd01@gmail.com>
2023-03-13 11:34:02 +0700

    test Streetview function

--- test.py
+++ test.py
@@ -8,47 +8,58 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import folium
 
-import streetview
-
-chars = "abcdefghijklmnopqrstuvwxyz0123456789"
-
-xdc = "_xdc_._" + "".join([y for x in range(6) if (y := choice(chars)) is not None])
+from streetview import urls, geo
 
 # Effiel Tower Coordinate 
 lat = 48.8583701
 lng = 2.2944813
 
+lat = 21.01931
+lng = 105.830341
+
 radius = 500
 
 map_center = [lat, lng]
 map_zoom = 15
-map = folium.Map(location=map_center, zoom_start=map_zoom)
+
+url = urls._build_metadata_url(lat=lat, lng=lng, mode="SingleImageSearch", radius=radius)
+json_data = requests.get(url).text
+if "Search returned no images." in json_data:
+    print("[google]: Finding nearest panorama via satellite zoom...")
+    url = urls._build_metadata_url(lat=lat, lng=lng, mode="SatelliteZoom")
+    json_data = requests.get(url).text
+    data = json.loads(json_data[4:])
+    print(data)
+    # pano = data[1][1][0][0][0][1]
+
+
+print(pano)
+# map = folium.Map(location=map_center, zoom_start=map_zoom)
 
 # Add a marker to the map
-marker = folium.Marker(location=map_center, tooltip='My Location')
-marker.add_to(map)
+# marker = folium.Marker(location=map_center, tooltip='My Location')
+# marker.add_to(map)
 
 # Save the map as an HTML file
 
-delay=5
-fn='map.html'
-tmpurl='file://{path}/{mapfile}'.format(path=os.getcwd(),mapfile=fn)
-map.save(fn)
+# delay=5
+# fn='map.html'
+# tmpurl='file://{path}/{mapfile}'.format(path=os.getcwd(),mapfile=fn)
+# map.save(fn)
 
-options = Options()
-options.headless = True
+# options = Options()
+# options.headless = True
   
 # initializing webdriver for Chrome with our options
-browser = webdriver.Firefox(options=options)
+# browser = webdriver.Firefox(options=options)
 
-browser.get(tmpurl)
+# browser.get(tmpurl)
 
-delay=5
-#Give the map tiles some time to load
-time.sleep(delay)
+# delay=5
+# time.sleep(delay)
 
-browser.save_screenshot('map.png')
-browser.quit()
+# browser.save_screenshot('map.png')
+# browser.quit()
 
 # def download_pano(panoid, filename, zoom=3):
 #     panorama = streetview.download_panorama_v3(panoid, zoom=zoom, disp=False, filename=filename, alternate=False)
