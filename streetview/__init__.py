2c4f32a88e9643fa8144c5cbba0fac79a4fb55d7
aidenpearce001 <aidenpearcewd01@gmail.com>
2023-03-13 18:34:04 +0700

    uncomment _panoids_url

--- streetview/__init__.py
+++ streetview/__init__.py
@@ -93,17 +93,17 @@ class geo:
         tile_y = math.floor((y * zoom) / tile_size)
         return tile_x, tile_y
 
-# def _panoids_url(lat, lon):
+def _panoids_url(lat, lon):
 
-#     """
-#     Builds the URL of the script on Google's servers that returns the closest
-#     panoramas (ids) to a give GPS coordinate.
-#     """
-#     url = ("https://maps.googleapis.com/maps/api/js/GeoPhotoService.SingleImageSearch?"
-#            "pb=!1m5!1sapiv3!5sUS!11m2!1m1!1b0!2m4!1m2!3d{0:}!4d{1:}!2d50!3m10!2m2!1sen!2sGB!9m1!1e2!11m4!1m3!1e2!2b1!3e2!4m10!1e1!1e2!1e3!1e4!1e8!1e6!5m1!1e2!6m1!1e2"
-#            "&callback=_xdc_._v2mub5")
+    """
+    Builds the URL of the script on Google's servers that returns the closest
+    panoramas (ids) to a give GPS coordinate.
+    """
+    url = ("https://maps.googleapis.com/maps/api/js/GeoPhotoService.SingleImageSearch?"
+           "pb=!1m5!1sapiv3!5sUS!11m2!1m1!1b0!2m4!1m2!3d{0:}!4d{1:}!2d50!3m10!2m2!1sen!2sGB!9m1!1e2!11m4!1m3!1e2!2b1!3e2!4m10!1e1!1e2!1e3!1e4!1e8!1e6!5m1!1e2!6m1!1e2"
+           "&callback=_xdc_._v2mub5")
     
-#     return url.format(lat, lon)
+    return url.format(lat, lon)
 
 
 def _panoids_data(lat, lon, proxies=None):
